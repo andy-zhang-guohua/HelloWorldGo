513b9dc34d3e851ead136c600c094ff7672a0941
ZhangGuohua <786188617@qq.com>
2021-01-02 11:48:53 +0800

    channel without buffer

--- main.go
+++ main.go
@@ -1,48 +1,18 @@
 package main // 要求这里报名必须是 main
+
 import (
 	"fmt"
-	"reflect"
-	"strconv"
+	"runtime"
 )
 
-type Element interface{}
-type List [] Element
-
-type Person struct {
-	name string
-	age int
-}
-
-// 定义了 String 方法，实现了 fmt.Stringer
-func (p Person) String() string {
-	return "(name: " + p.name + " - age: "+strconv.Itoa(p.age)+ " years)"
-}
-
-func testReflect(variable Element){
-	t := reflect.TypeOf(variable)    // 得到类型的元数据,通过t我们能获取类型定义里面的所有元素
-	v := reflect.ValueOf(variable)
-
-	fmt.Printf("type : %v\n", t)
-	fmt.Printf("value : %v\n", v)
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		runtime.Gosched() // 表示让 CPU 把时间片让给别人，下次某个时候继续恢复执行该 goroutine
+		fmt.Println(s)
+	}
 }
 
 func main() {
-	list := make(List, 3)
-	list[0] = 1 // an int
-	list[1] = "Hello" // a string
-	list[2] = Person{"Dennis", 70}
-
-	for index, element := range list {
-		if value, ok := element.(int); ok {
-			fmt.Printf("list[%d] is an int and its value is %d\n", index, value)
-		} else if value, ok := element.(string); ok {
-			fmt.Printf("list[%d] is a string and its value is %s\n", index, value)
-		} else if value, ok := element.(Person); ok {
-			fmt.Printf("list[%d] is a Person and its value is %s\n", index, value)
-		} else {
-			fmt.Printf("list[%d] is of a different type\n", index)
-		}
-	}
-
-	testReflect(list[1]);
+	go say("world") // 开一个新的 Goroutines 执行
+	say("hello") // 当前 Goroutines 执行
 }
