c374c47a802b5e50170d2d784f26dfc47a2f2b29
ZhangGuohua <786188617@qq.com>
2021-01-02 15:57:03 +0800

    HTTP服务器例子

--- main.go
+++ main.go
@@ -2,28 +2,33 @@ package main // 要求这里报名必须是 main
 
 import (
 	"fmt"
-	"runtime"
+	"net/http"
+	"strings"
+	"log"
 )
 
-func main() {
-	c := make(chan int, 1) // 修改 2 为 1 就报错，修改 2 为 3 可以正常运行
-
-	c <- 1
-
-	go func() {
-		fmt.Println("in goroutine", <-c)
-
-		fmt.Println("CPU core count : ",runtime.NumCPU())
-
-		fmt.Println("goroutine count : ",runtime.NumGoroutine())
-	}()
-
-	c <- 2
-
-	fmt.Println(<-c)
-
-	fmt.Println("main goroutine count : ",runtime.NumGoroutine())
+func sayhelloName(responseWriter http.ResponseWriter, request *http.Request) {
+	request.ParseForm()                                // 解析参数，默认是不会解析的
+	fmt.Println("request parameters : ", request.Form) // 这些信息是输出到服务器端的打印信息
+	fmt.Println("path : ", request.URL.Path)
+	fmt.Println("scheme : ", request.URL.Scheme)
+	fmt.Println("request parameter : url_long = ", request.Form["url_long"])
+	for k, v := range request.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ","))
+	}
+	fmt.Fprintf(responseWriter, "你好，浏览器!") // 这个写入到 responseWriter 的是输出到客户端的
 }
 
-// 修改为 1 报如下的错误:
-// fatal error: all goroutines are asleep - deadlock!
+/**
+访问测试 :
+1. 启动本程序
+2. 访问地址 http://localhost:9090/?a=123&a=32&url_long=10000
+*/
+func main() {
+	http.HandleFunc("/", sayhelloName)       // 设置访问的路由
+	err := http.ListenAndServe(":9090", nil) // 设置监听的端口
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
