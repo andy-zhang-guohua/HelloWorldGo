7d744e7b03e8523e26d86b3b8889c4e7540af711
ZhangGuohua <786188617@qq.com>
2021-01-02 15:33:34 +0800

    goroutine runtime

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main // 要求这里报名必须是 main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 func main() {
-	c := make(chan int, 2) // 修改 2 为 1 就报错，修改 2 为 3 可以正常运行
+	c := make(chan int, 1) // 修改 2 为 1 就报错，修改 2 为 3 可以正常运行
+
 	c <- 1
+
+	go func() {
+		fmt.Println("in goroutine", <-c)
+
+		fmt.Println("CPU core count : ",runtime.NumCPU())
+
+		fmt.Println("goroutine count : ",runtime.NumGoroutine())
+	}()
+
 	c <- 2
+
 	fmt.Println(<-c)
-	fmt.Println(<-c)
+
+	fmt.Println("main goroutine count : ",runtime.NumGoroutine())
 }
+
 // 修改为 1 报如下的错误:
 // fatal error: all goroutines are asleep - deadlock!
